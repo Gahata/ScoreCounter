af9a086430856be7154504292d9349a1b3c60c69
Paweł <p.jezierski999@gmail.com>
2017-11-14 17:21:47 +0100

    Added initial recycler code, it's not working stuff Added counter cardview, initial version only

---
+++ app/src/main/java/com/example/pawel/scorecounter/CounterRecyclerAdapter.java
@@ -0,0 +1,68 @@
+package com.example.pawel.scorecounter;
+
+import android.content.Intent;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+/**
+ * Created by Pawel on 14.11.2017.
+ */
+
+public class CounterRecyclerAdapter extends RecyclerView.Adapter<CounterRecyclerAdapter.RecentsRecyclerViewHolder> {
+
+
+    public CounterRecyclerAdapter (){
+    }
+
+    @Override
+    public CounterRecyclerAdapter.RecentsRecyclerViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View listItem = LayoutInflater.from(parent.getContext()).inflate(R.layout.note_card, parent, false);
+        return new RecentsRecyclerViewHolder(listItem);
+    }
+
+    @Override
+    public void onBindViewHolder(CounterRecyclerAdapter.RecentsRecyclerViewHolder holder, int position) {
+        holder.titleCardTextView.setText(noteList.get(position).getTitle());
+        holder.contentCardTextView.setText(noteList.get(position).getContent());
+    }
+
+    @Override
+    public int getItemCount() {
+        return noteList.size();
+    }
+
+    public static class RecentsRecyclerViewHolder extends RecyclerView.ViewHolder {
+        TextView nameTextView;
+        TextView scoreTextView;
+
+
+        public RecentsRecyclerViewHolder(final View itemView) {
+            super(itemView);
+            nameTextView =itemView.findViewById(R.id.title_note_card);
+            scoreTextView =itemView.findViewById(R.id.content_preview_note_card);
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    int position = getAdapterPosition();
+                    Intent intent = new Intent(itemView.getContext(), NoteActivity.class);
+                    intent.putExtra("NOTE", noteList.get(position));
+                    intent.putExtra("ID", noteList.get(position).getId());
+                    itemView.getContext().startActivity(intent);
+
+                }
+            });
+        }
+
+
+    }
+
+
+
+
+}
--- app/src/main/java/com/example/pawel/scorecounter/MainActivity.java
+++ app/src/main/java/com/example/pawel/scorecounter/MainActivity.java
@@ -13,6 +13,8 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.view.menu.ActionMenuItemView;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.support.v7.widget.AppCompatImageView;
 import android.view.View;
@@ -30,6 +32,10 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
 
     TextView toolbarTextView;
+
+    CounterRecyclerAdapter counterRecyclerAdapter;
+    LinearLayoutManager linearLayoutManager;
+    RecyclerView recyclerCounters;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
